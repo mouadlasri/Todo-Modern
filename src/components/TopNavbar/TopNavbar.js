5a5c47e62ada2c6fe9fde90be46214a63df015ec
Mouad Lasri <=>
2023-02-19 09:22:01 +0900

    Added sidebar menus and topnav title

--- src/components/TopNavbar/TopNavbar.js
+++ src/components/TopNavbar/TopNavbar.js
@@ -4,8 +4,12 @@ import "./TopNavbar.css";
 
 export function TopNavbar() {
   return (
-    <div className="top-navbar-container">
-      <div className="top-navbar"></div>
+    <div className="top-navbar-container flex">
+      <div className="top-navbar">
+        <div className="top-navbar-branding">
+          <span>Modern To Do App made with React</span>
+        </div>
+      </div>
       <div></div>
       <div></div>
     </div>
