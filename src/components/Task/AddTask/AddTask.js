ca53fcf52c70e090ae9a6a42f15ded885719c538
Mouad Lasri <mouadkor20@gmail.com>
2023-02-25 02:21:42 +0900

    Add circle next to Add Task on the left

--- src/components/Task/AddTask/AddTask.js
+++ src/components/Task/AddTask/AddTask.js
@@ -20,6 +20,23 @@ export function AddTask() {
   return (
     <div className="mt-auto">
       <div className="w-full add-task-default">
+        <span className="task-checkbox">
+          <svg
+            class="fluentIcon ___12fm75w f1w7gpdv fez10in fg4l7m0"
+            fill="#2564cf"
+            aria-hidden="true"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            focusable="false"
+          >
+            <path
+              d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z"
+              fill="#2564cf"
+            ></path>
+          </svg>
+        </span>
         {/* <h1>Add Task</h1> */}
         <form onSubmit={handleSubmit} style={{ width: "100%" }}>
           <input
